39b975ba6cb769e13f68b138d6fadc150eb935de
Pawel Kacela <pawel.kacela@gmail.com>
2026-02-11 18:54:19 +0100

    change starting view

--- SideProjectTCA/AppWrapper/AppWrapper.swift
+++ SideProjectTCA/AppWrapper/AppWrapper.swift
@@ -7,20 +7,29 @@
 
 import SwiftUI
 import ComposableArchitecture
+import TodoList
 
 @main
 public struct AppWrapper: App {
     
     public init() { }
     
-    private let store = Store(
-        initialState: WrReducer.State(),
-        reducer:  { WrReducer() }
+//    private let store = Store(
+//        initialState: WrReducer.State(),
+//        reducer:  { WrReducer() }
+//    )
+    
+    let store = Store(
+        initialState: TaskListReducer.State(),
+        reducer: {
+            TaskListReducer()
+        }
     )
     
    public var body: some Scene {
         WindowGroup {
-            WrView(store: store)
+//            WrView(store: store)
+            TodoListView(store: store)
         }
     }
 }
