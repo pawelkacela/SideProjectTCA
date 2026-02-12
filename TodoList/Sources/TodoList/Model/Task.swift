6e04c0ca96e29b9f30c9592bb6d362b2c998232f
Pawel Kacela <pawel.kacela@gmail.com>
2026-02-12 18:49:49 +0100

    add, sendable protocol

--- TodoList/Sources/TodoList/Model/Task.swift
+++ TodoList/Sources/TodoList/Model/Task.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Task: Identifiable, Equatable, Codable {
+public struct Task: Identifiable, Equatable, Codable, Sendable {
     public var id: UUID = .init()
     public var name: String
     public var dateCreated: Date
